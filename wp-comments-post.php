3659334e393ca2b05d9dc418b88cbb177e0bbd4f
steve <withliterature@gmail.com>
2024-05-17 11:46:03 +0900

    aaa

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -9,6 +9,7 @@ if ( 'POST' !== $_SERVER['REQUEST_METHOD'] ) {
 	$protocol = $_SERVER['SERVER_PROTOCOL'];
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0', 'HTTP/3' ), true ) ) {
 		$protocol = 'HTTP/1.0';
+		
 	}
 
 	header( 'Allow: POST' );
