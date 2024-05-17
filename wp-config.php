ca94d2e1666e74a4ddca7795709cb7374506ce0e
steve <withliterature@gmail.com>
2024-05-17 10:31:02 +0900

    123
    123

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wd-php' );
+define( 'DB_NAME', 'wd-php' ); //0517
 
 /** Database username */
 define( 'DB_USER', 'root' );
